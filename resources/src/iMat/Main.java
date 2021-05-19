12e91edeb57cbf7e7b102238a711e6ca4777edf8
Isabella <fraansson@gmail.com>
2021-05-19 15:22:01 +0200

    Create payment-branch
    Initial layout
    * Added back button
    * Added logo-image

---
+++ resources/src/iMat/Main.java
@@ -0,0 +1,23 @@
+package iMat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("iMat_front.fxml"));
+        primaryStage.setTitle("iMat");
+        primaryStage.setScene(new Scene(root, 1980, 1080));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
