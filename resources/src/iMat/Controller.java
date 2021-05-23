f7a94f8869568e09267733d5811e5610b99a62c2
Daniel Bylinka <daniel.bylinka@gmail.com>
2021-05-23 19:35:08 +0200

    Add user's page

--- resources/src/iMat/Controller.java
+++ resources/src/iMat/Controller.java
@@ -1,6 +1,7 @@
 package iMat;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import se.chalmers.cse.dat216.project.ProductCategory;
+import se.chalmers.cse.dat216.project.*;
 
 import java.lang.reflect.Array;
 import java.util.List;
@@ -17,9 +18,6 @@ import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.util.Callback;
-import se.chalmers.cse.dat216.project.IMatDataHandler;
-import se.chalmers.cse.dat216.project.Order;
-import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.net.URL;
 import java.util.*;
@@ -29,12 +27,24 @@ public class Controller implements Initializable {
     @FXML private FlowPane orderHistoryFlowPane;
     @FXML private AnchorPane storeAnchorPane;
     @FXML private AnchorPane orderHistoryAnchorPane;
+    @FXML private AnchorPane myPageAnchorPane;
     @FXML private AnchorPane orderHistoryLightbox;
     @FXML private AnchorPane categoryAnchorPane;
     @FXML private FlowPane orderHistoryDetailFlowPane;
     @FXML private AnchorPane orderItemHeader;
     @FXML private Label orderHistoryLightboxHeader;
     @FXML private VBox categoryListFlowPane;
+    @FXML private GridPane myPageHome;
+    @FXML private GridPane myPageRecords;
+
+    @FXML private TextField myPageRecordsAddress;
+    @FXML private TextField myPageRecordsEmail;
+    @FXML private TextField myPageRecordsFirstName;
+    @FXML private TextField myPageRecordsLastName;
+    @FXML private TextField myPageRecordsPhoneNumber;
+    @FXML private TextField myPageRecordsMobileNumber;
+    @FXML private TextField myPageRecordsPostAddress;
+    @FXML private TextField myPageRecordsPostCode;
     private final Map<Integer, List<OrderItem>> orderItemMap = new HashMap<>();
 
     private IMatDataHandler handler = IMatDataHandler.getInstance();
@@ -43,6 +53,7 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initializeOrderHistory();
         updateCategoryList();
+        initializeMyPageRecords();
     }
 
     private void updateCategoryList(){
@@ -72,6 +83,18 @@ public class Controller implements Initializable {
         orderHistoryLightbox.toBack();
     }
 
+    private void initializeMyPageRecords() {
+        Customer customer = IMatDataHandler.getInstance().getCustomer();
+        myPageRecordsAddress.setText(customer.getAddress());
+        myPageRecordsEmail.setText(customer.getEmail());
+        myPageRecordsFirstName.setText(customer.getFirstName());
+        myPageRecordsLastName.setText(customer.getLastName());
+        myPageRecordsPhoneNumber.setText(customer.getPhoneNumber());
+        myPageRecordsMobileNumber.setText(customer.getMobilePhoneNumber());
+        myPageRecordsPostAddress.setText(customer.getPostAddress());
+        myPageRecordsPostCode.setText(customer.getPostCode());
+    }
+
     public void onClickOrderHistory(Order order) {
         populateOrderHistoryLightbox(order);
         orderHistoryLightbox.toFront();
@@ -85,9 +108,35 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    public void onClickOrderLightbox(Event event) {
+    public void goToOrders(Event event) {
         orderHistoryAnchorPane.toFront();
         storeAnchorPane.toFront();
+    }
+
+    @FXML
+    public void goToMyPage(Event event) {
+        myPageAnchorPane.toFront();
+        myPageHome.toFront();
+        storeAnchorPane.toFront();
+    }
+
+    @FXML
+    public void editUserRecords(Event event) {
+        myPageRecords.toFront();
+    }
+
+    @FXML
+    public void saveUserRecords(Event event) {
+        Customer customer = IMatDataHandler.getInstance().getCustomer();
+        customer.setAddress(myPageRecordsAddress.getText());
+        customer.setEmail(myPageRecordsEmail.getText());
+        customer.setFirstName(myPageRecordsFirstName.getText());
+        customer.setLastName(myPageRecordsLastName.getText());
+        customer.setPhoneNumber(myPageRecordsPhoneNumber.getText());
+        customer.setMobilePhoneNumber(myPageRecordsMobileNumber.getText());
+        customer.setPostAddress(myPageRecordsPostAddress.getText());
+        customer.setPostCode(myPageRecordsPostCode.getText());
+        System.out.println("Set the customer's records.");
     }
 
     @FXML
